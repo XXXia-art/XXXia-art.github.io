d6e1d5e704f124f99b6050342434f44b986446c7
XXXia-art <pz.xia@qq.com>
2026-06-06 16:43:46 +0800

    添加了自己的vr项目

--- data/site-content.js
+++ data/site-content.js
@@ -143,6 +143,7 @@
       title: "News",
       type: "timeline",
       items: [
+        { date: "<em>[2026.06]</em>", text: " I used Unity to create a VR version of the gun practice software🎯." },
         { date: "<em>[2026.05]</em>", text: " I am currently developing <em><strong>a grand board game</strong></em>💰." },
         { date: "<em>[2026.04]</em>", text: " Building my <em><strong>personal website</strong></em> on GitHub Pages." },
         { date: "<em>[2026.03]</em>", text: " Working on <em><strong>sequential unlearning</strong></em> methods for diffusion models." },
